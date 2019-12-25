b4098a2c35a76a6f4ff2d6d50b06ffc42dc56071
Lisa Lundquist <lisamarielundquist@yahoo.com>
2019-12-24 17:56:42 -0700

    PasswordGenerator

--- script.js
+++ script.js
@@ -1,12 +1,129 @@
-var passwordLength = prompt ("How long would you like your password to be? Please enter a number.");
-console.log(passwordLength)
+var passLength = prompt ("How many characters would you like your password to be?");
+var passNumbers = confirm ("Would you like your password to contain Numbers?");
+var passSpecial = confirm ("Would you like your password to contain special characters?");
+var passUpperCase  = confirm ("Would you like your password to contain Uppercase letters?");
+var passLowerCase = confirm ("Would you like your password to contain Lowercase letters" );
+var generatedPassword = ""
+var numChars = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+];
+var specialChars = [
+    "~",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+];
+var upperChars = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G", 
+    "H", 
+    "I", 
+    "J", 
+    "K", 
+    "L", 
+    "M", 
+    "N", 
+    "O", 
+    "P", 
+    "Q", 
+    "R", 
+    "S", 
+    "T", 
+    "U", 
+    "V", 
+    "W", 
+    "X", 
+    "Y", 
+    "Z",
+];
 
-function generate() 
-  let complexity = console.log(passwordLength).value;
-  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
-  let password = "";
- 
- for var i = 0; i < complexity; i++){
-     password = password + values.charAt(math.floor(Math.random() * Math.floor(values.length-1)))
-    }
-  document.getElementById("card-body").value=password;
+var lowerChars = [
+    "a", 
+    "b", 
+    "c", 
+    "d", 
+    "e", 
+    "f", 
+    "g", 
+    "h", 
+    "i", 
+    "j", 
+    "k", 
+    "l", 
+    "m", 
+    "n", 
+    "o", 
+    "p", 
+    "q", 
+    "r", 
+    "s", 
+    "t", 
+    "u", 
+    "v", 
+    "w", 
+    "x",
+    "y", 
+    "z"
+];
+function getRandomInt(arr) {
+    var randomIndex =  Math.floor(Math.random() * arr.length);
+    var randElem = arr[randomIndex];
+    return randElem;
+  };
+function generatePass() {
+
+var userSelections = [];
+if (passNumbers === true){
+    userSelections.push(numChars)
+};
+if (passLowerCase === true){
+    userSelections.push(lowerChars)
+} 
+if (passSpecial ===true){
+    userSelections.push(specialChars)
+};
+if (passUpperCase ===true){
+    userSelections.push(upperChars)
+};
+
+for (var i = 0; i < passLength; i++) {
+  generatedPassword = generatedPassword  + getRandomInt(getRandomInt(userSelections));
+}
+return generatedPassword
+};
+function displayPassword() {
+   console.log("functionRan"); 
+   document.getElementById("passwordDisplay").textContent  =  generatePass()
+}
+console.log(generatePass());
+console.group("userSelections");
+console.log(passLength);
+console.log(passNumbers);
+console.log(passSpecial);
+console.log(passUpperCase);
+console.log(generatedPassword);
+console.groupEnd();
+
+
+
